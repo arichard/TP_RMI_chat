412e1195144332e2b8aebed0f6ea4b8faf17757a
Antoine Richard <ant.richard@gmail.com>
2011-12-02 17:26:45 +0100

    Suppression

--- TP_RMI_chat/src/Chat.java
+++ TP_RMI_chat/src/Chat.java
@@ -1,11 +1,50 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
- * 
- * Interface pour le chat utilisant l'API RMI
- * @author Antoine
+ * Classe qui implemente le client
  *
  */
-public interface Chat extends java.rmi.Remote {
+public class Chat_client extends UnicastRemoteObject implements ClientInterface{
+
+	public Chat_client () throws java.rmi.RemoteException {
+		super();
+	}
+
+	public void notifie(String nomUtilisateur, String phrase) {
+		System.out.println(nomUtilisateur+":"+phrase);
+	}
+
+	public void startClient() throws IOException{
+		BufferedReader standard = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (true) {
+		    	String ligne = standard.readLine();
+		    	System.out.println("Got line "+ligne);
+		    		if (ligne.equals("/quit")) {
+		    			return;
+		    		}
+		 }
+	}
 	
+	
+	public static void main(String[] args) {
+		
+		try {
+            Chat_client monClient = new Chat_client();
+            java.rmi.Naming.rebind("chatclient", monClient);
+            System.out.println("Le client de chat a ete enregistre " + "dans le serveur de nom.");
+            monClient.startClient();
+		} 
+		
+		catch (Exception e) {
+            System.err.println("Exception dans Client : " + e.getMessage());
+            e.printStackTrace();
+        }
+   
+	}
 }
+
